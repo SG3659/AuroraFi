1e6912395201b08a8a01c78e119d9cb15d0d8009
Sahil Gupta <sahilgupta43384@gmail.com>
2026-01-15 23:10:37 +0530

    set up frontend

---
+++ Client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className=" text-6xl bg-slate-600">hello</h1>
+      <Button> click me</Button>
+
+    </>
+  )
+}
+
+export default App
