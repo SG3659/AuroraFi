ba52a84b55b667b8b8bc0431590d9437865f7bbf
Sahil Gupta <sahilgupta43384@gmail.com>
2026-07-30 13:44:12 +0530

    refactore : remove server code

--- Server/src/routes/analytic.routes.ts
+++
@@ -1,8 +0,0 @@
-import { summaryAnalyticsController, chartAnalyticsController, expensePieChartBreakdownController } from "../controllers/analytic.controller.js";
-import { Router } from "express";
-import AuthMiddleware from "../middleware/auth.middleware.js";
-const analyticsRoutes = Router();
-analyticsRoutes.get("/summary", AuthMiddleware, summaryAnalyticsController);
-analyticsRoutes.get("/chart", AuthMiddleware, chartAnalyticsController);
-analyticsRoutes.get("/expense-breakdown", AuthMiddleware, expensePieChartBreakdownController);
-export default analyticsRoutes;
