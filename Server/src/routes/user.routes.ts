e0f069e4820b520077ed48739d9891fb0e2b3d69
Sahil Gupta <sahilgupta43384@gmail.com>
2026-08-13 17:49:34 +0530

    refactor: male the avatar code workable

--- Server/src/routes/user.routes.ts
+++ Server/src/routes/user.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express"
 import AuthMiddleware from "../middleware/auth.middleware.js"
 import { getUserProfileController, updateUserProfileController } from "../controllers/user.controller.js";
-
+import { upload } from "../config/cloudnary.config.js";
 const userRouter = Router()
 userRouter.get("/profile", AuthMiddleware, getUserProfileController)
-userRouter.put("/profile", AuthMiddleware, updateUserProfileController)
+userRouter.put("/update", AuthMiddleware, upload.single("profilePicture"), updateUserProfileController)
 export default userRouter
