cd319a21a9d9e26ba4e9d60cffb76fe0af27fcd8
Sahil Gupta <sahilgupta43384@gmail.com>
2026-09-26 16:55:25 +0530

    remove the space

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -20,9 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(
-   cors({
-
-   })
+   cors({})
 );
 app.use(apiLimiter)
 app.use("/api/v1", authRouter)
